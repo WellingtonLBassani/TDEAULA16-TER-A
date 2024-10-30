402b7d4da7f98b57410ab571cace08c5be0aa517
Wellington <wellington.bassani28@gmail.com>
2024-10-29 21:24:50 -0300

    first commit

---
+++ Configuracoes.cs
@@ -0,0 +1,6 @@
+public static class Configuracoes
+{
+    public static string NomeAplicacao { get; set; }
+    public static string Versao { get; set; }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine(ConversorTemperatura.CelsiusParaFahrenheit(39));
+Console.WriteLine(ConversorTemperatura.FahrenheitParaCelsius(102.2));
+
+
+Configuracoes.NomeAplicacao = "Laboratorio de programação";
+Configuracoes.Versao = "2";
+
+Console.WriteLine(Configuracoes.NomeAplicacao);
+Console.WriteLine(Configuracoes.Versao);
